d444bad83e609ba8b6c7871fc7f1cf66ac478729
Mohamed Azab <monmody@gmail.com>
2023-09-09 08:12:02 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints the lowercase alphabet in reverse
+ * Return:: 0
+ */
+int main(void)
+
+{
+	int z;
+
+	for (z = '0'; z <= '9'; z++)
+	putchar(z);
+	putchar('\n');
+	return (0);
+}
